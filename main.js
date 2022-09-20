85d4607c8d71ccd3cc1c11e88a6a18f417198a4e
zakirovravshan <zakirovrovshan@gmail.com>
2022-09-20 18:15:20 +0500

    voice-todo

---
+++ main.js
@@ -0,0 +1,26 @@
+var elform = document.querySelector('.form');
+var elinp = document.querySelector('.inp');
+var elsubmit = document.querySelector('.submit');
+var elrecord = document.querySelector('.record');
+var ellist = document.querySelector('.list')
+
+elform.addEventListener('submit', function (evt) {
+    evt.preventDefault()
+    var elinpvalue = elinp.value ;
+    newitem = document.createElement('li');
+    newitem.textContent = elinpvalue;
+    ellist.appendChild(newitem);
+    elinp.value = " "
+
+})
+
+var record = new webkitSpeechRecognition();
+record.lang = 'uz-UZ'
+
+elrecord.addEventListener('click', function () {
+    record.start();
+    document.body.record.style.backgroundColor = 'red';
+ })
+ record.onresult = function (evt) {
+	elinp.value = evt.results[0][0].transcript;}
+
